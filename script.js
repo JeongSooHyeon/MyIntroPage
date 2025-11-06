3f0e0b16c4d742414d16330d300885d6c0e263ed
JeongSooHyeon <jsh75721@naver.com>
2025-11-06 16:17:30 +0900

    week2-day2 랜덤 인사말, 반복문

---
+++ script.js
@@ -0,0 +1,22 @@
+// 오늘의 인사말 랜덤 alert
+const greetings = ["안녕", "안녕하세요", "달룡하세요", "달선하세요", "달례하세요"];
+
+function randGreeting() {
+    const rand = Math.floor(Math.random() * greetings.length);
+    alert(greetings[rand] + "~");
+}
+
+window.onload = function () {
+    randGreeting();
+};
+
+// 반복문 연습
+const mungs = [
+    {name : "달룡", hobby:"간식 바라보기"},
+    {name : "달선", hobby:"코 골기"},
+    {name : "달례", hobby:"바짓가랭이 물고 끌려댕기기"},
+]
+
+for(let dog of mungs){
+    console.log(`${dog.name}의 취미는 ${dog.hobby}예요.`);
+}
